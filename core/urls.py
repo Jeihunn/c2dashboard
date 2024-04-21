cd2fdc8bc55dee2dceffda78e1c78b598f5e031f
Jeyhun Piriyev <jeihunpiriyev@gmail.com>
2024-04-21 18:37:49 +0400

    start, stop server

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import start_server
+from .views import start_server, stop_server
 
 urlpatterns = [
     path('', start_server, name='start_server'),
+    path('stop-server/', stop_server, name='stop_server'),
 ]
