ca599e9ab26a2d9a77c384bff5621c0a35b0b75c
Jeyhun Piriyev <jeihunpiriyev@gmail.com>
2024-04-21 19:57:26 +0400

    ui updated

--- c2dashboard/settings.py
+++ c2dashboard/settings.py
@@ -118,16 +118,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-
-from django.contrib.messages import constants as messages
-MESSAGE_TAGS = {
-        messages.DEBUG: 'alert-secondary',
-        messages.INFO: 'alert-info',
-        messages.SUCCESS: 'alert-success',
-        messages.WARNING: 'alert-warning',
-        messages.ERROR: 'alert-danger',
-}
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
